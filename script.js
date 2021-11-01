40a1dd114009f8b983fe674215b6f431b3511221
Weaknessxyz <eliasknks@gmail.com>
2021-11-02 00:27:16 +0100

    Fix remaining bugs, add backspace

--- script.js
+++ script.js
@@ -3,17 +3,27 @@ let operand1 = null;
 let operand2 = null;
 let currOperator = null;
 
-setDisplayValue();
-setOperator();
-placeDigit();
-getResult();
+start();
+
+function setDisplayValue() {
+    if (displayValue === 'ERROR' || isNaN(displayValue)) {
+        document.querySelector('.display-bar span').textContent = 'ERROR';
+        operand1 = null;
+        operand2 = null;
+        currOperator = null;
+        displayValue = 0;
+        
+    } else {
+        document.querySelector('.display-bar span').textContent = Number(displayValue);
+    }
+}
 
 function placeDigit() {
     const operands = document.querySelectorAll('.operand');
     operands.forEach(button => {
         button.addEventListener('click', e => {
-            displayValue += e.target.textContent;
-            setDisplayValue();
+                displayValue += e.target.textContent;
+                setDisplayValue();
         });
     });
 }
@@ -43,11 +53,29 @@ function setOperator() {
     });
 }
 
+function clearAll() {
+    const clearBtn = document.querySelector('#clear');
+    clearBtn.addEventListener('click', () => {
+        displayValue = '0';
+        operand1 = null;
+        operand2 = null;
+        currOperator = null;
+        setDisplayValue();
+    });
+}
 
+function delDigit() {
+    const delBtn = document.querySelector('#backspace');
+    delBtn.addEventListener('click', () => {
+        console.log('hi');
+        displayValue = displayValue.toString().slice(0, -1);
+        setDisplayValue();
+    });
+}
 
 function getResult() {
-    const equals = document.querySelector('#equals');
-    equals.addEventListener('click', e => {
+    const equalsBtn = document.querySelector('#equals');
+    equalsBtn.addEventListener('click', () => {
 
         if (!operand1) {
             return
@@ -60,6 +88,15 @@ function getResult() {
         operand1 = null;
         operand2 = null;
     });
+}
+
+function start() {
+    setDisplayValue();
+    setOperator();
+    placeDigit();
+    getResult();
+    clearAll();
+    delDigit();
 }
 
 function add(num1, num2) {
@@ -75,7 +112,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        return 'ERROR'
+    } else return num1 / num2;
 }
 
 function operate(operator, num1, num2) {
@@ -93,8 +132,3 @@ function operate(operator, num1, num2) {
             return divide(num1, num2);
     }
 }
-
-function setDisplayValue() {
-    document.querySelector('.display-bar span').textContent = Number(displayValue);
-}
-
