cce380ce2b3b143ffc9b7c2a826c6c7ae3dedcc1
weakness <eliasknks@gmail.com>
2021-11-03 10:40:15 +0100

    Change font and remove functions for simple calculations

--- script.js
+++ script.js
@@ -5,37 +5,21 @@ let currOperator = null;
 
 start();
 
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    if (num2 === 0) {
-        return 'ERROR'
-    } else return Math.round(num1 / num2 * 100000) / 100000;
-}
-
 function operate(operator, num1, num2) {
     switch (operator) {
         case '+':
-            return add(num1, num2);
+            return num1 + num2;
             break;
         case '-':
-            return subtract(num1, num2);
+            return num1 - num2;
             break;
         case '*':
-            return multiply(num1, num2);
+            return num1 * num2;
             break;;
         case '/':
-            return divide(num1, num2);
+            if (num2 === 0) {
+                return 'ERROR'
+            } else return Math.round(num1 / num2 * 100000) / 100000;
     }
 }
 
@@ -99,7 +83,6 @@ function clearAllBtn() {
 function delDigit() {
     const delBtn = document.querySelector('#backspace');
     delBtn.addEventListener('click', () => {
-        console.log('hi');
         displayValue = displayValue.toString().slice(0, -1);
         setDisplayValue();
     });
